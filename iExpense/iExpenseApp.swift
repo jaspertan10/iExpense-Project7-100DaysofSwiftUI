c40c7ac02c632730feb375491cd1dcbb5dda05c0
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2024-11-27 21:53:16 -0800

    Initial commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Jasper Tan on 11/27/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
